50cbf7aff5ecc41943d461fceb9d994c3cbf43fc
julian-yamori <julian.lampyridae@gmail.com>
2024-02-10 02:08:34 +0900

    TrackListQueryService クラスを廃止して関数化

--- src/app/tracks/page.tsx
+++ src/app/tracks/page.tsx
@@ -1,6 +1,6 @@
 import PageTitle from "@/components/page_title";
 import prismaClient from "@/db/prisma_client";
-import TrackListQueryService from "@/db/track_list/track_list_query_service";
+import { queryAllTracks } from "@/db/track_list/track_list_query_service";
 import TracksTable from "./tracks_table";
 import createMetadata from "../_util/create_metadata";
 
@@ -11,8 +11,7 @@ export const metadata = createMetadata(PAGE_TITLE);
 
 /** 曲リストページ */
 export default async function Home() {
-  const tracksQueryService = new TrackListQueryService(prismaClient);
-  const tracks = await tracksQueryService.allTracks();
+  const tracks = await queryAllTracks(prismaClient);
 
   return (
     <main>
--- src/db/track_list/track_list_query_service.ts
+++ src/db/track_list/track_list_query_service.ts
@@ -4,26 +4,24 @@ import { difficultyFromNum } from "@/domain/track/difficulty";
 import { PrismaTransaction } from "../prisma_client";
 import { TrackListDto } from "./track_list_dto";
 
-/** 曲一覧のDB参照機能 */
-export default class TrackListQueryService {
-  constructor(public readonly prismaTransaction: PrismaTransaction) {}
-
-  async allTracks(): Promise<TrackListDto[]> {
-    return (
-      await this.prismaTransaction.track.findMany({
-        where: { deleted: false },
-        include: { scores: { select: { difficulty: true, lv: true } } },
-      })
-    ).map((dbModel) => ({
-      id: dbModel.id,
-      title: dbModel.title,
-      skillType: skillTypeFromNum(dbModel.skillType),
-      long: dbModel.long,
-      openType: openTypeFromNum(dbModel.openType),
-      scores: dbModel.scores.map((d) => ({
-        difficulty: difficultyFromNum(d.difficulty),
-        lv: d.lv,
-      })),
-    }));
-  }
+// eslint-disable-next-line import/prefer-default-export -- じきに関数を追加予定
+export async function queryAllTracks(
+  tx: PrismaTransaction,
+): Promise<TrackListDto[]> {
+  return (
+    await tx.track.findMany({
+      where: { deleted: false },
+      include: { scores: { select: { difficulty: true, lv: true } } },
+    })
+  ).map((dbModel) => ({
+    id: dbModel.id,
+    title: dbModel.title,
+    skillType: skillTypeFromNum(dbModel.skillType),
+    long: dbModel.long,
+    openType: openTypeFromNum(dbModel.openType),
+    scores: dbModel.scores.map((d) => ({
+      difficulty: difficultyFromNum(d.difficulty),
+      lv: d.lv,
+    })),
+  }));
 }
