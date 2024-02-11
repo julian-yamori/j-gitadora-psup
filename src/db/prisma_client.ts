70b93d4b0f9e04b66f0d006d520bd87158a21f70
julian-yamori <julian.lampyridae@gmail.com>
2024-02-11 17:22:43 +0900

    next dev で PrismaClient インスタンスが複製される問題を回避

--- src/db/prisma_client.ts
+++ src/db/prisma_client.ts
@@ -1,8 +1,23 @@
 import "server-only";
 import { PrismaClient } from "@prisma/client";
 
-const prismaClient = new PrismaClient();
+// next dev でインスタンスが複製される問題の回避
+// https://www.prisma.io/docs/orm/more/help-and-troubleshooting/help-articles/nextjs-prisma-client-dev-practices
+
+const prismaClientSingleton = () => new PrismaClient();
+
+declare global {
+  // eslint-disable-next-line vars-on-top, no-var
+  var prismaClient: undefined | ReturnType<typeof prismaClientSingleton>;
+}
+
+const prismaClient = globalThis.prismaClient ?? prismaClientSingleton();
+
 export default prismaClient;
+
+if (process.env.NODE_ENV !== "production") {
+  globalThis.prismaClient = prismaClient;
+}
 
 /** PrismaClientとトランザクションのどちらも受け付けられる型 */
 export type PrismaTransaction = Omit<
