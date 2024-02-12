fe9a87ea4ffb607042b9830818377d1d09deaeed
julian-yamori <julian.lampyridae@gmail.com>
2024-02-12 18:54:55 +0900

    ScoreOrder の型定義を zod に変更

--- src/domain/score_query/score_order.ts
+++ src/domain/score_query/score_order.ts
@@ -1,16 +1,22 @@
-export type ScoreOrder = {
-  target: ScoreOrderTarget;
-  direction: OrderDirection;
-};
+import { z } from "zod";
 
-export type ScoreOrderTarget =
-  | "title"
-  | "skillType"
-  | "long"
-  | "difficulty"
-  | "lv"
-  | "like"
-  | "achievement"
-  | "skillPoint";
+const scoreOrderTargetSchema = z.union([
+  z.literal("title"),
+  z.literal("skillType"),
+  z.literal("long"),
+  z.literal("difficulty"),
+  z.literal("lv"),
+  z.literal("like"),
+  z.literal("achievement"),
+  z.literal("skillPoint"),
+]);
+export type ScoreOrderTarget = z.infer<typeof scoreOrderTargetSchema>;
 
-export type OrderDirection = "asc" | "desc";
+const orderDirectionSchema = z.union([z.literal("asc"), z.literal("desc")]);
+export type OrderDirection = z.infer<typeof orderDirectionSchema>;
+
+export const scoreOrderSchema = z.object({
+  target: scoreOrderTargetSchema,
+  direction: orderDirectionSchema,
+});
+export type ScoreOrder = z.infer<typeof scoreOrderSchema>;
