47e45988cfb1911e2570794f505f4200ed9b6249
julian-yamori <julian.lampyridae@gmail.com>
2024-04-11 15:55:53 +0000

    CORS の設定を厳格化

--- next.config.js
+++ next.config.js
@@ -4,18 +4,20 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/js/:path*",
+        source: "/js/load_ohp_history.js",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://p.eagate.573.jp",
+          },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+            value: "GET,OPTIONS",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, authorization",
+            value: "authorization",
           },
         ],
       },
